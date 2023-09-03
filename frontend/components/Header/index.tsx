45a8ebb81dadb5b58cdc947bfa9910e98a15721c
Igor Strelnyk <59284261+rodriguezz4060@users.noreply.github.com>
2023-09-03 16:43:47 +0300

    авторизация
    авторизация

--- frontend/components/Header/index.tsx
+++ frontend/components/Header/index.tsx
@@ -1,57 +1,66 @@
-import React from "react"
-import {Button, Paper, IconButton, Avatar} from "@material-ui/core"
+import React from 'react'
+import { Button, Paper, IconButton, Avatar } from '@material-ui/core'
 import styles from './Header.module.scss'
-import SearchIcon from '@mui/icons-material/Search';
-import Link from 'next/link';
-import CreateIcon from '@mui/icons-material/CreateOutlined';
-import MessageIcon from '@mui/icons-material/SmsOutlined';
-import NotificationIcon from '@mui/icons-material/NotificationsOutlined';
-import Menu from '@mui/icons-material/MenuOutlined';
-import ArrowBottom from '@mui/icons-material/KeyboardArrowDownOutlined';
-import logo from "/pages/static/img/logo.svg"
-import Image from "next/image";
-
+import SearchIcon from '@mui/icons-material/Search'
+import Link from 'next/link'
+import CreateIcon from '@mui/icons-material/CreateOutlined'
+import MessageIcon from '@mui/icons-material/SmsOutlined'
+import NotificationIcon from '@mui/icons-material/NotificationsOutlined'
+import Menu from '@mui/icons-material/MenuOutlined'
+import ArrowBottom from '@mui/icons-material/KeyboardArrowDownOutlined'
+import logo from '/pages/static/img/logo.svg'
+import Image from 'next/image'
+import { AuthDialog } from '../AuthDialog'
 
 export const Header: React.FC = () => {
-    return (
-        <Paper classes={{root: styles.root}} elevation={0}>
-            <div className="d-flex align-center">
-                <IconButton>
-                    <Menu/>
-                </IconButton>
-                <Link href="/">
-                    <Image className={styles.logo} src={logo}/>
-                </Link>
-                <div className={styles.searchBlock}>
-                    <SearchIcon/>
-                    <input placeholder="Поиск"/>
-                </div>
+  const [authVisible, setAuthVisible] = React.useState(false)
 
-                <Link href={'/write'}>
-                    <Button variant="contained" className={styles.penBottom}>
-                        <CreateIcon/> Написать
-                    </Button>
-                </Link>
-            </div>
+  const handleClickOpen = () => {
+    setAuthVisible(true)
+  }
 
-            <div className="d-flex align-center">
-                <IconButton>
-                    <MessageIcon/>
-                </IconButton>
-                <IconButton>
-                    <NotificationIcon/>
-                </IconButton>
-                <Link href="/profile/1" className="d-flex align-center">
+  const handleClose = () => {
+    setAuthVisible(false)
+  }
 
-                    <Avatar
-                        className={styles.avatar}
-                        alt="Remy Sharp"
-                        src="https://leonardo.osnova.io/84acaa93-a48a-5e08-ba4f-79be1c92a724/-/scale_crop/108x108/-/format/webp/"/>
+  return (
+    <Paper classes={{ root: styles.root }} elevation={0}>
+      <div className='d-flex align-center'>
+        <IconButton>
+          <Menu />
+        </IconButton>
+        <Link href='/'>
+          <Image className={styles.logo} src={logo} alt='Logo' />
+        </Link>
+        <div className={styles.searchBlock}>
+          <SearchIcon />
+          <input placeholder='Поиск' />
+        </div>
 
-                    <ArrowBottom/>
+        <Link href={'/write'}>
+          <Button variant='contained' className={styles.penBottom}>
+            <CreateIcon /> Написать
+          </Button>
+        </Link>
+      </div>
 
-                </Link>
-            </div>
-        </Paper>
-    )
+      <div className='d-flex align-center'>
+        <IconButton onClick={handleClickOpen}>
+          <MessageIcon />
+        </IconButton>
+        <IconButton>
+          <NotificationIcon />
+        </IconButton>
+        <Link href='/profile/1' className='d-flex align-center'>
+          <Avatar
+            className={styles.avatar}
+            alt='Remy Sharp'
+            src='https://leonardo.osnova.io/84acaa93-a48a-5e08-ba4f-79be1c92a724/-/scale_crop/108x108/-/format/webp/'
+          />
+          <AuthDialog onClose={handleClose} visible={authVisible} />
+          <ArrowBottom />
+        </Link>
+      </div>
+    </Paper>
+  )
 }
--- frontend/components/SideComments/CommentItem.tsx
+++ frontend/components/SideComments/CommentItem.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import styles from './SideComments.module.scss'
+import Link from 'next/link'
 
 interface CommentItemProps {
   user: {
+    id: number
     fullname: string
   }
   text: string
   post: {
+    id: number
     title: string
   }
 }
@@ -15,15 +18,18 @@ export const CommentItem: React.FC<CommentItemProps> = ({ user, text, post }) =>
   return (
     <div className={styles.commentItem}>
       <div className={styles.userInfo}>
-        <img src='https://leonardo.osnova.io/598fc957-a3f6-598c-b6f9-a033c3941d12/-/scale_crop/64x64/-/format/webp/' />
-        <a href='#'>
+        <img
+          src='https://leonardo.osnova.io/598fc957-a3f6-598c-b6f9-a033c3941d12/-/scale_crop/64x64/-/format/webp/'
+          alt='Avatar'
+        />
+        <Link href={`/profile/${user.id}`}>
           <b>{user.fullname}</b>
-        </a>
+        </Link>
       </div>
       <p className={styles.text}>{text}</p>
-      <a href='#'>
+      <Link href={`/news/${post.id}`}>
         <span className={styles.postTitle}>{post.title}</span>
-      </a>
+      </Link>
     </div>
   )
 }
