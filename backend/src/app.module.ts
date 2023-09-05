61e79bc5ec56f34d6b0070f83460f74e2fbd0267
Igor Strelnyk <59284261+rodriguezz4060@users.noreply.github.com>
2023-09-05 19:50:29 +0300

    docker
    подключение docker

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,22 +4,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { UserEntity } from './user/entities/user.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+    UserModule,
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'Myangels9bb',
-      database: 'uajournal',
+      type: process.env.DB_TYPE as any,
+      host: process.env.PG_HOST,
+      port: parseInt(process.env.PG_PORT),
+      username: process.env.PG_USER,
+      password: process.env.PG_PASSWORD,
+      database: process.env.PG_DB,
       entities: [UserEntity],
       synchronize: true,
     }),
-    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
