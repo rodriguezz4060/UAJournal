cce3f83b1ace57ad118c414d803b29611060cc33
Igor Strelnyk <59284261+rodriguezz4060@users.noreply.github.com>
2023-09-09 13:07:22 +0300

    JWT авторизация
    Авторизация JWTпроверка валидности токена авторизации

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,19 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { UserEntity } from 'src/user/entities/user.entity';
 import { UserService } from 'src/user/user.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
   async validateUser(email: string, password: string): Promise<any> {
     const user = await this.userService.findByCont({
-        email,
-        password
+      email,
+      password,
     });
     if (user && user.password === password) {
       const { password, ...result } = user;
       return result;
     }
     return null;
+  }
+
+  async login(user: UserEntity) {
+    const { password, ...userData } = user;
+    const payload = { email: user.email, sub: user.id };
+    return {
+      ...userData,
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
---
+++ backend/src/auth/strategies/jwt.stategy.ts
@@ -0,0 +1,31 @@
+import { UserService } from 'src/user/user.service';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly userService: UserService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: 'test',
+    });
+  }
+  async validate(payload: { sub: number; email: string }) {
+    const data = { id: payload.sub, email: payload.email };
+
+    const user = await this.userService.findByCont(data);
+
+    if (!user) {
+      throw new UnauthorizedException('У вас нет доступа к этой странице');
+    }
+
+    return {
+      id: user.id,
+      email: user.email,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+    };
+  }
+}
