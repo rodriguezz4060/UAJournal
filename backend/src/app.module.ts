876754e521c0a8b29c4a10510c28cbcaa7a742c4
Igor Strelnyk <59284261+rodriguezz4060@users.noreply.github.com>
2024-04-17 11:01:56 +0300

    add env for server conect

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -11,28 +11,33 @@ import { CommentEntity } from './comment/entities/comment.entity'
 import { AuthModule } from './auth/auth.module'
 import { AwsController } from './aws/aws.controller'
 import { AwsModule } from './aws/aws.module'
-import { ConfigModule } from '@nestjs/config'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 import { RatingEntity } from './post/entities/rating.entity'
 import { RatingModule } from './rating/rating.module'
 import { FollowingEntity } from './user/entities/following.entity'
 
 @Module({
 	imports: [
-		TypeOrmModule.forRoot({
-			type: 'postgres',
-			host: '127.0.0.1',
-			port: 5432,
-			username: 'postgres',
-			password: 'Myangels9bb',
-			database: 'postgres',
-			entities: [
-				UserEntity,
-				PostEntity,
-				CommentEntity,
-				RatingEntity,
-				FollowingEntity
-			],
-			synchronize: true
+		ConfigModule.forRoot(), // Инициализация модуля конфигурации
+		TypeOrmModule.forRootAsync({
+			imports: [ConfigModule],
+			useFactory: (configService: ConfigService) => ({
+				type: 'postgres',
+				host: configService.get('DB_HOST'),
+				port: +configService.get<number>('DB_PORT'),
+				username: configService.get('DB_USERNAME'),
+				password: configService.get('DB_PASSWORD'),
+				database: configService.get('DB_DATABASE'),
+				entities: [
+					UserEntity,
+					PostEntity,
+					CommentEntity,
+					RatingEntity,
+					FollowingEntity
+				],
+				synchronize: true
+			}),
+			inject: [ConfigService]
 		}),
 		UserModule,
 		PostModule,
