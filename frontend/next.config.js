af36ec1fd619ad9249545dcc8f9c15ec701648f6
Igor Strelnyk <59284261+rodriguezz4060@users.noreply.github.com>
2024-04-18 15:07:41 +0300

    vercel

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,15 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-	env: {
-		NODE_ENV: process.env.NODE_ENV || 'development'
-	},
-	reactStrictMode: true,
-	swcMinify: true,
-	images: {
-		domains: [
-			'leonardo.osnova.io',
-			'uajournal-post.s3.amazonaws.com',
-			'uajournal-post.s3.eu-central-1.amazonaws.com'
-		]
+	webpack: (config, { dev, isServer }) => {
+		if (!dev && !isServer) {
+			config.optimization.minimize = false
+		}
+
+		return config
 	}
 }
