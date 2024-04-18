a3120a6f137243c90e08159fbdc28b3deb5099c5
Igor Strelnyk <59284261+rodriguezz4060@users.noreply.github.com>
2024-04-18 14:10:16 +0300

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ['leonardo.osnova.io', 'uajournal-post.s3.amazonaws.com', 'uajournal-post.s3.eu-central-1.amazonaws.com'],
-  },
+	env: {
+		NODE_ENV: process.env.NODE_ENV || 'development'
+	},
+	reactStrictMode: true,
+	swcMinify: true,
+	images: {
+		domains: [
+			'leonardo.osnova.io',
+			'uajournal-post.s3.amazonaws.com',
+			'uajournal-post.s3.eu-central-1.amazonaws.com'
+		]
+	}
 }
